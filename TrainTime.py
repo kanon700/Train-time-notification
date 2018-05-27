e9a97b3a0a28d1c03a84cd3c062c551e31693533
kanon700 <kanon700@yahoo.co.jp>
2018-05-27 21:57:59 +0900

    Provisional completion

--- TrainTime.py
+++ TrainTime.py
@@ -9,55 +9,33 @@ def getNearestValue(list, num):
     return list[idx]
 
 if __name__ == "__main__":
-
-    url = "https://roote.ekispert.net/ja/timetable/25000/1350"
+    # 到着予定時刻は現在時間+walktimeの時間
+    walktime=15
     date = datetime.datetime.now()
-    walktime= '00:15:00'
+    date+=datetime.timedelta(minutes=walktime)
     today=datetime.date.today()
 
-    # ttime = datetime.datetime.strptime(test, '%H:%M:%S')
-    # print(ttime)
-
+    # 駅すぱーとから時刻表を取得する
+    url = "https://roote.ekispert.net/ja/timetable/25000/1350"
     fetched_df = pandas.io.html.read_html(url,index_col=0)
-    df=fetched_df[0]
-    list=[]
 
+    # 0が平日 1が土日祝
+    df=fetched_df[0]
+    # 取得した時刻をdatetime形式でlist化する
+    list=[]
     for index, row in df.iterrows():
         test=df.at[index,1]
         test = test.split()
         for item in test:
             item=re.sub(r'\D', '',item)
-            # print(str(index)+':'+str(item))
             ttime=datetime.datetime.strptime(str(datetime.date.today())+' '+str(index)+':'+str(item)+':00', '%Y-%m-%d %H:%M:%S')
             list.append(ttime)
 
-    # print(list)
-
-    # idx = np.abs((date - np.asarray(list)).argmin())
-    # print(np.asarray(list) - date)
-    # print(np.abs(date - list[90]).argmin)
-    # print(date - list[90])
-    # print(date)
-    # print(list[90])
-
+    # リストと到着予定時刻から最短で乗れる電車の時刻を取得
     for val in list:
         if (date < val) :
             print(val)
             break
-
-
-    # 0が平日 1が土日祝
-    # timetable =fetched_df[0]
-    # test=df.at[5,1]
-    # test = test.split()
-    # test = [int(s) for s in test]
-    # print(getNearestValue(test,30))
-
-    #fetched_df = fetched_df.split(' ')
-    #print(df.index.values)
-    #print(df.columns.values)
-
-    # print(lists)
 
     # 0が平日 1が土日祝
     # fetched_df[0].to_csv('output0.csv',index='false', encoding="utf-8")
